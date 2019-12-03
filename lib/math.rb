8b24f343aebcc5ccb065e740e1108036642a22df
Iqra Masroor <masrooriqra@gmail.com>
2019-12-03 20:25:03 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,24 @@
 def addition(num1, num2)
-
+  sum = num1+num2
+  sum
 end
 
 def subtraction(num1, num2)
-
+  deference = num1-num2
 end
 
 def division(num1, num2)
-
+  divedend = num1 / num2
 end
 
 def multiplication(num1, num2)
-
+  multiple = num1 * num2 
 end
 
 def modulo(num1, num2)
-
+  divide = num2 / num1
 end
 
 def square_root(num)
-
+  sqr=num**
 end
